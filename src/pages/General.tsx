8f9736c96e2d5061679d9871b76ce95ebb0b5664
GeorgeVorob <george77019721@gmail.com>
2022-01-31 15:40:10 +0700

    leave contacts form handle

--- src/pages/General.tsx
+++ src/pages/General.tsx
@@ -9,17 +9,59 @@ import Project from "../models/models";
 import { getProjects } from "../api/api";
 
 type generalState = {
-    projects: Project[];
+    projects: Project[],
+    formName: string,
+    formSurname: string,
+    formEmail: string,
+    formAddress: string,
+    formPhone: string;
 }
 
 function General() {
 
-    const [state, setState] = useState<generalState>({ projects: [] });
+    const [state, setState] = useState<generalState>({
+        projects: [],
+        formName: "",
+        formSurname: "",
+        formEmail: "",
+        formAddress: "",
+        formPhone: ""
+    });
 
     useEffect(() => {
         getProjects().then(res => setState({ ...state, projects: res }));
     }, [])
 
+    const sendContacts = (e: React.FormEvent): void => {
+        e.preventDefault();
+        console.log("data:", e);
+        fetch("/live_contact", {
+            method: 'POST',
+            body: JSON.stringify({
+                name: state.formName,
+                surname: state.formSurname,
+                email: state.formEmail,
+                address: state.formAddress,
+                phone: state.formPhone,
+            }),
+        })
+    }
+
+    const formNameHandle = (e: any): void => {
+        setState({ ...state, formName: e.target.value });
+    }
+    const formSurnameHandle = (e: any): void => {
+        setState({ ...state, formSurname: e.target.value });
+    }
+    const formEmailHandle = (e: any): void => {
+        setState({ ...state, formEmail: e.target.value });
+    }
+    const formAddressHandle = (e: any): void => {
+        setState({ ...state, formAddress: e.target.value });
+    }
+    const formPhoneHandle = (e: any): void => {
+        setState({ ...state, formPhone: e.target.value });
+    }
 
     return (
         <>
@@ -35,27 +77,28 @@ function General() {
                 <form
                     style={{ float: "right", marginRight: 81, maxWidth: "220px" }}
                     className="transparent-form"
+                    onSubmit={sendContacts}
                 >
                     <p className="main-font mb-4 ml-2 mr-2">оставить контакты</p>
                     <Form.Group>
                         <Form.Label className="mb-1">Имя</Form.Label>
-                        <Form.Control type="text"></Form.Control>
+                        <Form.Control type="text" onChange={(e) => formNameHandle(e)}></Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="mb-1">Фамилия</Form.Label>
-                        <Form.Control type="text"></Form.Control>
+                        <Form.Control type="text" onChange={(e) => formSurnameHandle(e)}></Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="mb-1">Эл. почта *</Form.Label>
-                        <Form.Control type="text" required></Form.Control>
+                        <Form.Control type="text" required onChange={(e) => formEmailHandle(e)}></Form.Control>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label className="mb-1">Адрес</Form.Label>
-                        <Form.Control type="text"></Form.Control>
+                        <Form.Control type="text" onChange={(e) => formAddressHandle(e)}></Form.Control>
                     </Form.Group>
                     <Form.Group className="mb-1">
                         <Form.Label className="mb-1">Номер телефона</Form.Label>
-                        <Form.Control type="text"></Form.Control>
+                        <Form.Control type="text" onChange={(e) => formPhoneHandle(e)}></Form.Control>
                     </Form.Group>
                     <button className="btn formBtn main-font-1" style={{ width: "100%" }}>Отправить</button>
                 </form>
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use( //TODO: make it better
-        createProxyMiddleware(['/getImage', '/getProject'],
+        createProxyMiddleware(['/getImage', '/getProject', '/live_contact'],
             {
                 target: 'http://localhost:3000',
                 changeOrigin: true,
